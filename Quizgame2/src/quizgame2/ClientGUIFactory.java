de42c40bba6fe3ceb0d1254701e5ecbf6024c987
ErikAneer <erik.aneer@yh.nackademin.se>
2018-11-23 15:04:55 +0100

    GUI methods added in ClientGUIFactory

--- Quizgame2/src/quizgame2/ClientGUIFactory.java
+++ Quizgame2/src/quizgame2/ClientGUIFactory.java
@@ -5,7 +5,7 @@ import java.io.ObjectOutputStream;
 public class ClientGUIFactory {
 
         ObjectOutputStream oos;
-//        ClientGUI clientgui = new ClientGUI(oos);
+        ClientGui clientgui = new ClientGui(oos);
 
         ClientGUIFactory(ObjectOutputStream oos) {
             this.oos = oos;
@@ -13,21 +13,20 @@ public class ClientGUIFactory {
 
         public void handleInputFromServer(String messageFromServer) {
                     if(messageFromServer.substring(0, 1).trim().equalsIgnoreCase("0")){ //fått koppling med servern. Visa startskärm för att kunna skicka namn till servern.
-
+                              clientgui.showStartScene();
                     }
                     else if (messageFromServer.substring(0, 1).trim().equalsIgnoreCase("1")) { // Väntar på motståndare // väntar på motståndare väljer kategori.
-
-
+                              clientgui.showWaitScene();
                     }
                     else if (messageFromServer.substring(0, 1).trim().equalsIgnoreCase("2")) { // Välja kategori.
-
+                              clientgui.showCategoryScene();
                     }
 
                     else if (messageFromServer.substring(0, 1).trim().equalsIgnoreCase("3")) { // Svara fråga / frågeGUI
-
+                              clientgui.showQuestionScene();
                     }
                     else if (messageFromServer.substring(0, 1).trim().equalsIgnoreCase("4")) { // Skapa scen rondpaus / spel klart.
-
+                              clientgui.showEndScene();
                     }
                     // Någon mer scen?
         }
